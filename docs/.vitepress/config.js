ae167558f7a03d159ec8408a4d35a070341c53e7
染川 瞳 <61381142+mirai-mamori@users.noreply.github.com>
2022-12-12 13:27:16 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress';
 
 
 export default defineConfig({
-  base: '/docs_site/',
   title: 'Fuukei 周边文档',
     themeConfig: {
       logo: 'https://s.nmxc.ltd/sakurairo_wiki/icon.png',
