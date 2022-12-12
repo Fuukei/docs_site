c3b7028c123e57c41bc503ec57e0bcc09ff984b3
mirai-mamori <mirai-mamori@users.noreply.github.com>
2022-12-12 08:51:53 +0000

    deploy: 0da6a2e384d53f3566ad9bf9be1f7fe58fad4de3

--- assets/Sakurairo_Templates_index.md.cd4ab5a7.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./app.f203afb1.js";const m=JSON.parse('{"title":"\u6A21\u7248","description":"","frontmatter":{"title":"\u6A21\u7248"},"headers":[],"relativePath":"Sakurairo/Templates/index.md"}'),r={name:"Sakurairo/Templates/index.md"};function o(n,s,c,i,p,d){return a(),t("div")}const l=e(r,[["render",o]]);export{m as __pageData,l as default};
