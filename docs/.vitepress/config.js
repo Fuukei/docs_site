df4abb70d4cb628e35c303e5e0de91aa30b0fa75
楠ちゃん <kusu@kusu.moe>
2023-01-01 15:50:53 +0800

    Ver 0.5.5: Something interesting.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Fuukei 周边文档',
   head:[
-    ['link', { rel: 'icon', href: 'https://s.nmxc.ltd/fuukei_docs/sakurairo/favicon.ico' }]
+    ['link', { rel: 'icon', href: 'https://s.nmxc.ltd/fuukei_docs/sakurairo/favicon.ico' }],
+    ["script", { src: './js/main.js'}]
     ],
     themeConfig: {
       logo: 'https://s.nmxc.ltd/fuukei_docs/sakurairo/icon.png',
