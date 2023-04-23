3a72e50b1487beecdbb6902fb59b998c006ddbb4
Shiroiame-Kusu <Shiroiame-Kusu@users.noreply.github.com>
2023-04-23 01:43:57 +0000

    deploy: 92b8225f42e52448b0e2506284d01922027798df

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 var demo = document.getElementsByClassName("Demo")
+function handleRouting() {
+    var AccessPath = window.location.pathname;
+    switch (AccessPath){
+        case '/secret':
+                var ClassChange = document.getElementsByClassName('NotFound')[0];
+                ClassChange.getElementsByClassName('code')[0].innerHTML = "000"
+                ClassChange.getElementsByClassName('title')[0].innerHTML = "你不该来这里"
+                fetch('https://v1.hitokoto.cn?min_length=20&max_length=32')
+                    .then(response => response.json())
+                        .then(data => {
+                            ClassChange.getElementsByClassName('quote')[0].innerHTML = data.hitokoto + '&nbsp;----&nbsp;'+ data.from
+                            })
+                    .catch(console.error)
+
+                ClassChange.getElementsByClassName('action')[0].getElementsByClassName('link')[0].innerHTML =  "回头吧，孩子"
+            break;
+        case '/secret.html':
+            break;
+        default:
+            break;
+    }
+}
+
+handleRouting();
+
+window.addEventListener("hashchange", handleRouting)
